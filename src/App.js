2bf46ca7438808e10a7475c0c72751f8157d11a0
avinash2wards <avinash2wards@outlook.com>
2023-01-20 21:02:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = React.useState(null);
+
+  useEffect(() => {
+    fetch(`process.env.api_url`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products ? (
+        products.map((product) => {
+          return (
+            <div key={product.name}>
+              <h1>{product.name}</h1>
+              <h1>{product.description}</h1>
+            </div>
+          );
+        })
+      ) : (
+        <h1>Loading....</h1>
+      )}
     </div>
   );
 }
